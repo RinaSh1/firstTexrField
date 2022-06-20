889f71763805b48634de24250a46a7a6328a659b
RinaSh1 <92697073+RinaSh1@users.noreply.github.com>
2022-06-20 21:52:54 +0200

    commit

--- firstTexrField/ViewController.swift
+++ firstTexrField/ViewController.swift
@@ -1,19 +1,51 @@
-//
-//  ViewController.swift
-//  firstTexrField
-//
-//  Created by Rina Shabani on 20.6.22.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let myField = UITextField(frame:CGRect(x: 100, y: 400,width: 210,height:  35))
+    let label = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 21))
+    let button = UIButton(frame: CGRect(x: 150, y:500, width:  100, height:  35))
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .gray
+        
+        /* label */
+        
+        label.center = CGPoint(x: 185, y: 285)
+        label.textAlignment = .center
+        label.text = "Enter Your Name"
+        
+        self.view.addSubview(label)
+        
+        
+        /* textfield */
+        myField.backgroundColor = .white
+        
+        self.view.addSubview(myField)
+        
+        /* button */
+        button.setTitleColor(UIColor.white, for: .normal)
+        button.setTitle("Hello", for: .normal)
+        button.backgroundColor = .systemPink
+        
+        button.addTarget(self, action: #selector(self.pressed), for: .touchUpInside)
+        
+        view.addSubview(button)
     }
-
-
+    
+    @objc func pressed() {
+        let name = myField.text!
+        
+        let greeting = "Hello, \(name)!";
+        label.text = greeting;
+        myField.resignFirstResponder();
+        // myField.text = "Hello " + name + " ;) "
+    }
+    
 }
+
 
