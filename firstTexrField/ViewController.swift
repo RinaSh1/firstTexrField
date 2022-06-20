102bff01573910b2981a1561d703dcdd3b7a5bca
RinaSh1 <92697073+RinaSh1@users.noreply.github.com>
2022-06-20 21:48:36 +0200

    Initial Commit

---
+++ firstTexrField/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  firstTexrField
+//
+//  Created by Rina Shabani on 20.6.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
